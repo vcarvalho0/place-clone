36cbfe23fea90c230f0e15b3c6bd36d7db1f8a87
vcarvalho0 <vitor1.santos00@gmail.com>
2024-08-31 22:33:36 -0300

    feat: remove useless code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,29 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 
-/*
-  const onMouseDownPan = (evt: React.MouseEvent) => {
-    const { clientX, clientY } = getMousePosition(evt)
-    setIsPanning(true)
-    setStartPanPosition({ x: clientX, y: clientY })
-  }
-
-  const onMouseMovePan = (evt: React.MouseEvent) => {
-    if (isPanning) {
-      const { clientX, clientY } = getMousePosition(evt)
-
-      const offsetX = clientX - startPanPosition.x + pan.x
-      const offsetY = clientY - startPanPosition.y + pan.y
-
-      setPan({ x: offsetX, y: offsetY })
-    }
-  }
-
-  const onMouseUpPan = () => {
-    setIsPanning(false)
-  }
-*/
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
