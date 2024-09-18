4e8e97c9e01ff4e4f942105e9d976c7b89fd28d0
vcarvalho0 <vitor1.santos00@gmail.com>
2024-09-18 14:22:17 -0300

    refactor: loading animation and add missing .env example

--- client/src/components/Loading/index.tsx
+++ client/src/components/Loading/index.tsx
@@ -1,59 +1,22 @@
+import { useEffect, useState } from "react"
 import * as S from "./style"
-
-function Spinner() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-    >
-      <g stroke="white">
-        <circle
-          cx="12"
-          cy="12"
-          r="6.5"
-          fill="none"
-          strokeLinecap="round"
-          strokeWidth="3"
-        >
-          <animate
-            attributeName="stroke-dasharray"
-            calcMode="spline"
-            dur="1.5s"
-            keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-            keyTimes="0;0.475;0.95;1"
-            repeatCount="indefinite"
-            values="0 150;42 150;42 150;42 150"
-          />
-          <animate
-            attributeName="stroke-dashoffset"
-            calcMode="spline"
-            dur="1.5s"
-            keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-            keyTimes="0;0.475;0.95;1"
-            repeatCount="indefinite"
-            values="0;-16;-59;-59"
-          />
-        </circle>
-        <animateTransform
-          attributeName="transform"
-          dur="2s"
-          repeatCount="indefinite"
-          type="rotate"
-          values="0 12 12;360 12 12"
-        />
-      </g>
-    </svg>
-  )
-}
+import { pallete } from "@/utils/pallete"
 
 export default function Loading() {
+  const [colorIndex, setColorIndex] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % pallete.length)
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   return (
     <>
-      <Spinner />
+      <S.Square backgroundColor={pallete[colorIndex].hex} />
       <S.Text>Loading...</S.Text>
-      <S.Text>It can take a while because the server is bad :(</S.Text>
     </>
   )
 }
--- client/src/components/Loading/style.ts
+++ client/src/components/Loading/style.ts
@@ -1,7 +1,26 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+type Square = {
+  backgroundColor: string
+}
+
+const expand = keyframes`
+  0%  { transform: scale(1, 1) }
+  80% { transform: scale(1.4, 1.4) }
+  100% { transform: scale(1, 1) }
+`
 
 export const Text = styled.p`
   font-family: "Silkscreen";
   color: white;
   font-size: 1.2rem;
+`
+
+export const Square = styled.div<Square>`
+  width: 30px;
+  height: 30px;
+  background: ${(props) => props.backgroundColor};
+  border: 3px solid black;
+  margin-bottom: 15px;
+  animation: ${expand} 1.5s infinite;
 `
